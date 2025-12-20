d7d059684efc817c4b3f6da9fb0219bc6fcbd4da
Pail-Dileep-Sai <dileepsaipaila@gmail.com>
2025-12-20 11:19:38 +0530

    graceful shutdown version-1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"market-analysis/internal/aggregate"
 	"market-analysis/internal/api"
 	"market-analysis/internal/ingest"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM) //context should be cancelled with ctrl+c or SIGTERM
+	defer stop()
+
 	aggregator := aggregate.NewAggregator()
 
 	log.Println("Starting data ingestion...")
@@ -20,7 +28,7 @@ func main() {
 		}
 	}()
 
-	// 3. Setup Server and Routes
+	// settingup server and routes
 	apiHandler := api.NewHandler(aggregator)
 
 	mux := http.NewServeMux()
@@ -43,11 +51,29 @@ func main() {
 		Handler:      mux,
 		ReadTimeout:  5 * time.Second, // timeouts to avoid resource leaks
 		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
 	}
 
-	// atart the server
-	log.Println("Server starting on port :8080...")
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatal(err)
+	//running on seperate goroutine so main-routine can block waiting for the signal
+	go func() {
+		log.Println("Server starting on port :8080...")
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
+
+	//block until Signal received
+	<-ctx.Done()
+	log.Println("Shutdown signal received. Closing server...")
+
+	// 7. Execute Graceful Shutdown
+	// Give the server 5 seconds to finish active requests
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Printf("Server forced to shutdown: %v", err)
 	}
+
+	log.Println("Server exited properly.")
 }
